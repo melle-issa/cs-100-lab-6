fd61d4af71dacf6901b7ac54f3e83cfb90c2766d
melle-issa <mhida010@ucr.edu>
2022-02-23 17:35:02 -0800

    added tests

--- test.cpp
+++ test.cpp
@@ -70,6 +70,57 @@ TEST(SelectContains, testSelectAnd){
     testSheet.print_selection(output);
     EXPECT_EQ(output.str(), "George 26 Riverside \n");
 }
+
+TEST(SelectContains, testSelectOr){
+    Spreadsheet testSheet;
+    testSheet.set_column_names({"Name","Age","City"});
+    testSheet.add_row({"George","26","Riverside"});
+    testSheet.add_row({"Percy","26","Seattle"});
+    testSheet.add_row({"Amelia","26","New York City"});
+    testSheet.set_selection(new Select_Or(new Select_Contains(&testSheet, "Name", "Amelia"), new Select_Contains(&testSheet, "City", "e")));
+    std::stringstream output;
+    testSheet.print_selection(output);
+    EXPECT_EQ(output.str(), "George 26 Riverside \nPercy 26 Seattle \nAmelia 26 New York City \n"); 
+}
+
+TEST(SelectContains, testSelectNotOr){
+    Spreadsheet testSheet;
+    testSheet.set_column_names({"Name","Age","City"});
+    testSheet.add_row({"George","26","Riverside"});
+    testSheet.add_row({"Percy","26","Seattle"});
+    testSheet.add_row({"Amelia","26","New York City"});
+    testSheet.set_selection(new Select_Not(new Select_Or(new Select_Contains(&testSheet, "Name", "a"), new Select_Contains(&testSheet, "Name", "y"))));
+    std::stringstream output;
+    testSheet.print_selection(output);
+    EXPECT_EQ(output.str(), "George 26 Riverside \n");
+}
+
+TEST(Select_Contains, testUpperCase){
+    Spreadsheet testSheet;
+    testSheet.set_column_names({"Name","Age","City"});
+    testSheet.add_row({"George","26","Riverside"});
+    testSheet.add_row({"Percy","26","Seattle"});
+    testSheet.add_row({"Amelia","26","New York City"});
+    testSheet.set_selection(new Select_Not(new Select_Contains(&testSheet, "City", "R")));
+    std::stringstream output;
+    testSheet.print_selection(output);
+    EXPECT_EQ(output.str(), "Percy 26 Seattle \nAmelia 26 New York City \n");
+}
+
+TEST(Select_Contains, testLowerCase){
+    Spreadsheet testSheet;
+    testSheet.set_column_names({"Name","Age","City"});
+    testSheet.add_row({"George","26","Riverside"});
+    testSheet.add_row({"Percy","26","Seattle"});
+    testSheet.add_row({"Amelia","26","New York City"});
+    testSheet.set_selection(new Select_Contains(&testSheet, "City", "s"));
+    std::stringstream output;
+    testSheet.print_selection(output);
+    EXPECT_EQ(output.str(), "George 26 Riverside \n");
+}
+
+TEST(
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
